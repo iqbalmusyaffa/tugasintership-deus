9872fa5351b538ff213a6a94a4fb2c16a84b0ad6
iqbalmusyaffa <yusa82401@gmail.com>
2024-05-12 21:32:07 +0700

    perbaikan code

---
+++ src/pages/Calendar.tsx
@@ -0,0 +1,12 @@
+import Breadcrumb from '../components/Breadcrumbs/Breadcrumb';
+import DefaultLayout from '../layout/DefaultLayout';
+
+const Calendar = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="Keuangan" />
+    </DefaultLayout>
+  );
+};
+
+export default Calendar;
